e49b11fc74124711098272d56c126968f4a0fa94
LegendMp374 <prajwalnayak17200@gmail.com>
2025-07-30 12:10:50 +0530

    Resolved merge conflicts in App.jsx and store.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import { configureStore } from "@reduxjs/toolkit";
+import attendanceReducer from "../components/Teacher/attendanceSlice";
+import studentReducer from "../components/Student/studentSlice";
 
-import LoginPage from "./components/Common/LoginPage";
-import StudentDashboard from "./components/Student/StudentDashboard";
-import TeacherDashboard from "./components/Teacher/TeacherDashboard";
-import AdminDashboard from "./components/Admin/AdminDashboard";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const App = () => {
-  return (
-    <Router>
-      <Toaster position="top-right" reverseOrder={false} />
+import { combineReducers } from "redux";
 
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/teacher" element={<TeacherDashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
-  );
+const rootReducer = combineReducers({
+  attendance: attendanceReducer,
+  student: studentReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["attendance"], // only persist attendance slice
 };
 
-export default App;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
