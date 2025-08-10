ce878c098b4c7296d1b17936f866944f709cac8d
LegendMp374 <prajwalnayak17200@gmail.com>
2025-08-10 20:51:38 +0530

    Added my updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import LoginPage from "./components/Common/LoginPage";
 import StudentDashboard from "./components/Student/StudentDashboard";
 import TeacherDashboard from "./components/Teacher/TeacherDashboard";
@@ -25,6 +27,8 @@ const App = () => {
 
   return (
     <Router>
+      <Toaster position="top-right" reverseOrder={false} />
+
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/student" element={<StudentDashboard />} />
